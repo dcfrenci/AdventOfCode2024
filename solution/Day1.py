527293ceeda4f678bb3ad413ff91185b9dff8c5b
dcfrenci <dcfrenci@gmail.com>
2024-12-02 20:25:58 +0100

    1 V2

--- solution/Day1.py
+++ solution/Day1.py
@@ -1,36 +1,26 @@
 import numpy as np
 
 
-def distance(lines):
-    firstColumn = []
-    secondColumn = []
+def one(lines):
+    first, second = [], []
     for line in lines:
-        split = line.split(" ")
-        firstColumn.append(split[0])
-        secondColumn.append(split[len(split) - 1])
-
-    firstColumn = sorted(np.array(firstColumn, dtype=int))
-    secondColumn = sorted(np.array(secondColumn, dtype=int))
-    results = 0
-    for i in range(len(firstColumn)):
-        results += abs(firstColumn[i] - secondColumn[i])
-    print(results)
+        first.append(int(line.split()[0]))
+        second.append(int(line.split()[-1]))
+    print(abs(np.array(sorted(first)) - np.array(sorted(second))).sum())
 
 
-def frequency(lines):
-    firstColumn = []
-    secondColumn = []
+def two(lines):
+    first, second = [], []
     for line in lines:
-        split = line.split(" ")
-        firstColumn.append(int(split[0]))
-        secondColumn.append(int(split[len(split) - 1]))
-    results = 0
-    for i in range(len(firstColumn)):
-        results += firstColumn[i] * secondColumn.count(firstColumn[i])
-    print(results)
+        first.append(int(line.split()[0]))
+        second.append(int(line.split()[-1]))
+    result = 0
+    for i in range(len(first)):
+        result += first[i] * second.count(first[i])
+    print(result)
 
 
 if __name__ == '__main__':
     fileInput = open("input/Day1.txt", "r").read()
-    distance(fileInput.split("\n"))
-    frequency(fileInput.split("\n"))
+    one(fileInput.split("\n"))
+    two(fileInput.split("\n"))
