bac5f22e4207ced1eda043d4133865a55e24dcb2
dcfrenci <dcfrenci@gmail.com>
2024-12-03 07:57:29 +0100

    3

---
+++ solution/Day3.py
@@ -0,0 +1,19 @@
+
+
+
+def one(line):
+
+
+
+
+
+def two(line):
+
+
+
+
+
+if __name__ == '__main__':
+    fileInput = open("input/Day2.txt", "r").read()
+    one(fileInput.split("\n"))
+    two(fileInput.split("\n"))
