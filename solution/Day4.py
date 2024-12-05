01b5bde54afd6709390b5105c09dc09c7cf86d88
dcfrenci2 <dcfrenci2@gmail.com>
2024-12-05 14:50:46 +0100

    4

--- solution/Day4.py
+++ solution/Day4.py
@@ -15,15 +15,18 @@ def calculate(matrix):
 def calc_diagonal(matrix):
     return (sum([len(re.findall(r"XMAS", "".join(np.diag(matrix, diagonal))) +
                      re.findall(r"SAMX", "".join(np.diag(matrix, diagonal)))) for diagonal in range(len(matrix))])
-             + sum([len(re.findall(r"XMAS", "".join(np.diag(matrix, diagonal - len(matrix))))
-                        + re.findall(r"SAMX", "".join(np.diag(matrix, diagonal - len(matrix))))) for diagonal in range(len(matrix))]))
+            + sum([len(re.findall(r"XMAS", "".join(np.diag(matrix, diagonal - len(matrix))))
+                       + re.findall(r"SAMX", "".join(np.diag(matrix, diagonal - len(matrix))))) for diagonal in
+                   range(len(matrix))]))
 
 
 def two(lines):
     matrix = np.array([list(line) for line in lines])
     sub_matrix_list = sliding_window_view(matrix, (3, 3)).reshape(-1, 3, 3)
-    return len([sub_matrix for sub_matrix in sub_matrix_list if "".join(np.diag(sub_matrix)) == "MAS" or "".join(np.diag(sub_matrix)) == "SAM"
+    return len([sub_matrix for sub_matrix in sub_matrix_list if
+                "".join(np.diag(sub_matrix)) == "MAS" or "".join(np.diag(sub_matrix)) == "SAM"
                 if "".join(np.diag(np.fliplr(sub_matrix))) == "MAS" or "".join(np.diag(np.fliplr(sub_matrix))) == "SAM"])
+
 
 if __name__ == '__main__':
     fileInput = open("input/Day4.txt", "r").read()
