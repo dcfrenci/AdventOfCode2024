b0010d28f8abab0ecb1856bc884fb2f3580f3ac4
dcfrenci2 <dcfrenci2@gmail.com>
2024-12-10 12:12:23 +0100

    7 v2

--- solution/Day7.py
+++ solution/Day7.py
@@ -1,61 +1,27 @@
-from itertools import product
-
-
 def one(lines):
-    results = 0
-    for line in lines:
-        test_value = int(line.split(':')[0])
-        numbers = [int(number) for number in line.split(':')[1].split()]
-        for string in product(['+', '*'], repeat=len(numbers) - 1):
-            if test_value == calculate(string, numbers):
-                results += test_value
-                break
-    print(results)
-
-
-def calculate(string, numbers):
-    results = numbers[0]
-    for index in range(len(numbers) - 1):
-        if string[index] == '+':
-            results += numbers[index + 1]
-        else:
-            results *= numbers[index + 1]
-    return results
+    print(sum(int(line.split(':')[0]) for line in lines
+              if int(line.split(':')[0]) in combination([int(number) for number in line.split(':')[1].split()], set(), False)))
 
 
 def two(lines):
-    results = 0
-    for line in lines:
-        test_value = int(line.split(':')[0])
-        numbers = [int(number) for number in line.split(':')[1].split()]
-        equation = False
-        for string in product(['+', '*'], repeat=len(numbers) - 1):
-            if test_value == calculate(string, numbers):
-                results += test_value
-                equation = True
-                break
-        if not equation:
-            for concatenate in combine(numbers, 0):
-                for string in product(['+', '*'], repeat=len(concatenate) - 1):
-                    if test_value == calculate(string, concatenate):
-                        results += test_value
-                        break
-    print(results)
+    print(sum(int(line.split(':')[0]) for line in lines
+              if int(line.split(':')[0]) in combination([int(number) for number in line.split(':')[1].split()], set(), True)))
 
 
-def combine(numbers, original):
-    combination = []
-    for i in range(len(numbers) - 1):
-        new_combination = (numbers[:i] if len(numbers[:i]) > 0 else []) + [] + (numbers[i + 2:] if len(numbers[i + 2:]) > 0 else [])
-        new_combination = [numbers[i] * pow(10, len(str(numbers[i + 1]))) + numbers[i + 1]]
-        combination.extend(new_combination.extend(combine(numbers[i + 2:], original)))
-        combination.extend(combine(numbers, original + 1))
-    if original == 0:
-        test = 0
-    return combination
+def combination(array, results, concatenation):
+    def combine(numbers, index, value):
+        if index + 1 >= len(numbers):
+            return results.add(value)
+        combine(numbers, index + 1, value * numbers[index + 1])
+        combine(numbers, index + 1, value + numbers[index + 1])
+        if concatenation:
+            combine(numbers, index + 1, int(str(value) + str(numbers[index + 1])))
+
+    combine(array, 0, array[0])
+    return results
 
 
 if __name__ == '__main__':
     fileInput = open("input/Day7.txt", "r").read()
-    # one(fileInput.split("\n"))
+    one(fileInput.split("\n"))
     two(fileInput.split("\n"))
